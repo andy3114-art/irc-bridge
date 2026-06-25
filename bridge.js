d16f20cc5bb6374322b87ec224d3a6e62f3caebe
andy3114-art <andy3114@gmail.com>
2026-06-24 21:48:39 -0400

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -1,15 +1,27 @@
 const WebSocket = require('ws');
 const net = require('net');
+const http = require('http');
 
-const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('IRC Bridge running');
+});
+
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
-  const irc = net.connect(80, 'irc-1x04.vaughnsoft.net');
+  console.log('Client connected');
+
+  const irc = net.connect(80, 'irc-1x04.vaughnsoft.net', () => {
+    console.log('Connected to Vaughn IRC');
+  });
+
   ws.on('message', (data) => irc.write(data.toString()));
   irc.on('data', (data) => { if (ws.readyState === 1) ws.send(data.toString()); });
   ws.on('close', () => irc.destroy());
   irc.on('close', () => ws.close());
-  irc.on('error', () => ws.close());
+  irc.on('error', (e) => { console.error(e.message); ws.close(); });
 });
 
-console.log('Bridge running');
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log('Bridge on port ' + PORT));
